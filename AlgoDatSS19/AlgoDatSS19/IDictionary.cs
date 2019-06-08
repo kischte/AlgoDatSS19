dfbeb4ad5c6124322c0938a848280ffe4437570f
kischte <45626128+kischte@users.noreply.github.com>
2019-06-08 21:11:49 +0200

    Klassen Arrays erstellt

---
+++ AlgoDatSS19/AlgoDatSS19/IDictionary.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoDatSS19
+{
+  interface IDictionary
+  {
+    bool Search(int x);
+    bool Insert(int x);
+    bool Delete(int x);
+    void Print();
+  }
+}
---
+++ AlgoDatSS19/AlgoDatSS19/SupportArray.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoDatSS19
+{
+  class SupportArray
+  {
+    public int[] array;
+
+    public SupportArray(int size)
+    {
+      this.array = new int[size];
+    }
+
+    public void Print()
+    {
+      for (int i = 0; i < array.Length; i++)
+      {
+        if (array[i] == 0)
+        {
+          Console.Write("[ ]" + " ");
+        }
+        else
+        {
+          Console.Write("[" + array[i] + "]" + " ");
+        }
+      }
+    }
+  }
+}
