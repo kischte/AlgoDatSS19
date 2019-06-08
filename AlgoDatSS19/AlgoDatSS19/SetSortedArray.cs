196a2d9eed31f1d8469e40cf601b1393f292a352
kischte <45626128+kischte@users.noreply.github.com>
2019-06-08 22:16:40 +0200

    ISetSorted & SetSortedArray erstellt

--- AlgoDatSS19/AlgoDatSS19/SetSortedArray.cs
+++ AlgoDatSS19/AlgoDatSS19/SetSortedArray.cs
@@ -6,8 +6,49 @@ using System.Threading.Tasks;
 
 namespace AlgoDatSS19
 {
-  class SetSortedArray
+  class SetSortedArray : MultiSetSortedArray, ISetSorted
   {
+    public SetSortedArray(int size) : base(size) { }
 
+    public new bool Insert(int x)
+    {
+      //leeres Element x wird nicht eingefügt
+      if (x == 0) 
+      {
+        return false;
+      }
+
+      //Wenn Element x schon vorhanden, wird es nicht neu hinzugefügt
+      if (Search(x)) 
+      {
+        return false;
+      }
+
+
+      for (int i = 0; i < array.Length; i++) 
+      {
+        if (array[i] > x)
+        {
+        //Sortiertes Einfügen von Element x an entprechender Stelle 
+          for (int pos = array.Length - 1; pos >= i + 1; pos--)
+          {
+          //Alle Elemente rechts von x um eins nach rechts verschieben
+            array[pos] = array[pos - 1];
+          }
+          array[i] = x;
+          return true;
+        }
+        else if (array[i] == 0)
+        {
+          array[i] = x;
+          return true;
+        }
+
+      }
+
+      return false;
+    }
+
+    //Print Funktion implementiert in SupportArray
   }
 }
