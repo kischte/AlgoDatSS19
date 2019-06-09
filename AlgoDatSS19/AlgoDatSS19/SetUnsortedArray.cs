31b364f56f5a475b9ad2464cb3d55a8a88b972e2
kischte <45626128+kischte@users.noreply.github.com>
2019-06-09 14:13:01 +0200

    SetSortedArray hinzugefügt

--- AlgoDatSS19/AlgoDatSS19/SetUnsortedArray.cs
+++ AlgoDatSS19/AlgoDatSS19/SetUnsortedArray.cs
@@ -6,7 +6,35 @@ using System.Threading.Tasks;
 
 namespace AlgoDatSS19
 {
-  class SetUnsortedArray
+  class SetUnsortedArray : MultiSetUnsortedArray, ISet
   {
+    public SetUnsortedArray(int size) : base(size) { }
+
+    public new bool Insert(int x)
+    {
+      //leeres Element x wird nicht hinzugefügt
+      if (x == 0)
+      {
+        return false;
+      }
+
+      //Prüfen ob Element x schon vorhanden ist, Menge -> nicht doppelt vorhanden
+      if (Search(x))
+      {
+        return false;
+      }
+
+      //Einfügen des Elements x 
+      for (int i = 0; i < array.Length; i++)
+      {
+        if (array[i] == 0)
+        {
+          array[i] = x;
+          return true;
+        }
+      }
+
+      return false;
+    }
   }
 }
