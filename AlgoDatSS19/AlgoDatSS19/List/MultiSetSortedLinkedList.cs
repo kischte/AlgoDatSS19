c64d1ad8c4191ec480cb357d687be62362f6c1bb
Masa-99 <44638953+Masa-99@users.noreply.github.com>
2019-06-11 11:23:21 +0200

    added MutiSetSortedLinkedList + file structure

---
+++ AlgoDatSS19/AlgoDatSS19/List/MultiSetSortedLinkedList.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoDatSS19
+{
+    class MultiSetSortedLinkedList : SupportList, IMultiSet
+    {
+        public override bool Insert(int node)
+        {
+            AddSorted(node);
+            return true;
+        }
+    }
+}
