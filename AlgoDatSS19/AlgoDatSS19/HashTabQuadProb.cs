84cf95491c20033c77932aac9c158f09b4c1c6ae
Masa-99 <44638953+Masa-99@users.noreply.github.com>
2019-06-11 12:36:55 +0200

    delete unnecessary using directives

--- AlgoDatSS19/AlgoDatSS19/HashTabQuadProb.cs
+++ AlgoDatSS19/AlgoDatSS19/HashTabQuadProb.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 //implements a quadratic probing hash table
 class HashTabQuadProb : AlgoDatSS19.ISet
--- AlgoDatSS19/AlgoDatSS19/List/MultiSetUnsortedList.cs
+++ AlgoDatSS19/AlgoDatSS19/List/MultiSetUnsortedList.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AlgoDatSS19
+﻿namespace AlgoDatSS19
 {
     class MultiSetUnsortedList : SupportList, IMultiSet
     {
